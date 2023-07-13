3a958c856b98d9fbda8f5e1f46fe7b9914a0800d
Jasonyecyec <jasonyecyec@gmail.com>
2023-07-13 14:14:55 +0800

    fix package

--- OtakuFlix/src/main.jsx
+++ OtakuFlix/src/main.jsx
@@ -1,16 +1,34 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
+// import { BrowserRouter } from "react-router-dom";
+// import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <SkeletonTheme baseColor="#313131" highlightColor="#525252">
+//     <React.StrictMode>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </React.StrictMode>
+//   </SkeletonTheme>
+// );
+
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <SkeletonTheme baseColor="#313131" highlightColor="#525252">
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.StrictMode>
-  </SkeletonTheme>
+  </SkeletonTheme>,
+  document.getElementById("root")
 );
