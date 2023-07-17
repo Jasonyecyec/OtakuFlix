49f1be5bc3a3200a234644b76c420a71ddf23633
Jasonyecyec <jasonyecyec@gmail.com>
2023-07-17 12:52:35 +0800

    fix

--- OtakuFlix/src/main.jsx
+++ OtakuFlix/src/main.jsx
@@ -1,20 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import "./index.css";
-// import { BrowserRouter } from "react-router-dom";
-// import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <SkeletonTheme baseColor="#313131" highlightColor="#525252">
-//     <React.StrictMode>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//     </React.StrictMode>
-//   </SkeletonTheme>
-// );
-
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
